35225608129c948f80679d858145e41f6a42f608
Juan Acosta <juan.acosta4@uac.edu.co>
2024-10-08 11:46:15 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = '01-first-steps';
+  name = "Juan Acosta";
+  imgRoute = "emisionsCO2.jpeg"
+  disabled = false
+  handleClick() {
+    console.log(`Hola ${this.name}`)
+  }
+  increment() {
+    this.count++
+  }
+  decrement() {
+    this.count--
+  }
+  nombreCompleto = ""
+  count = 0
+
 }
